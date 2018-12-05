fa76197b35acb769785ae860d86866ba5e36bd3f
carmona44 <daniel_95ca@hotmail.com>
2018-12-05 13:10:42 +0100

    Inicio proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Editor />
+        <Preview className="preview"/>
       </div>
+    );
+  }
+}
+
+class Editor extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+        <div className="editor">
+          <Toolbar title="Editor" clase="fas fa-code"/>
+        </div>
+    );
+  }
+}
+
+class Preview extends Component {
+  constructor(props){
+    super(props)
+  }
+  render (){
+    return (
+        <div className="preview">
+          <Toolbar title="Vista previa" clase="far fa-eye"/>
+        </div>
+    );
+  }
+}
+
+class Toolbar extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      title: props.title,
+      clase: props.clase
+    }
+  }
+  render(){
+    return (
+        <div className="toolbar">
+          <i className={this.state.clase}></i>
+          <span className="title">{this.state.title}</span>
+          <span className="max-icon"><i className="fas fa-expand-arrows-alt"></i></span>
+        </div>
     );
   }
 }
