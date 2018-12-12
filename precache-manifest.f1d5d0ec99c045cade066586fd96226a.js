e47c2fcd893933771c16b167297c59fb878cae61
carmona44 <daniel_95ca@hotmail.com>
2018-12-12 12:57:53 +0100

    Updates

---
+++ precache-manifest.f1d5d0ec99c045cade066586fd96226a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2c275135a06121482d1",
+    "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
+  },
+  {
+    "revision": "f9a59d6bfbb47f9a258f",
+    "url": "/markdown-previewer/static/js/main.f9a59d6b.chunk.js"
+  },
+  {
+    "revision": "016cf03f763747107210",
+    "url": "/markdown-previewer/static/js/1.016cf03f.chunk.js"
+  },
+  {
+    "revision": "f9a59d6bfbb47f9a258f",
+    "url": "/markdown-previewer/static/css/main.6ef40e99.chunk.css"
+  },
+  {
+    "revision": "04fc7300b02a29090da8423a661bafcb",
+    "url": "/markdown-previewer/index.html"
+  }
+];
